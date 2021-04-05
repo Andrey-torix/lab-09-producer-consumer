ee10ec8fa426c2f68f37e0259b498bd7c9bcae6a
legik777 <75944488+legik777@users.noreply.github.com>
2021-04-05 20:39:59 +0300

    Create suggestions.cpp

---
+++ sources/suggestions.cpp
@@ -0,0 +1,39 @@
+#include <boost/algorithm/string.hpp>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include "single_include\nlohmann\json.hpp"
+#include "suggestions.hpp"
+using namespace nlohmann;
+
+
+std::vector<suggestion> getFromJson(std::string path) {
+	std::vector<suggestion> v;
+	std::ifstream i(path);
+	json j;
+	i >> j;
+
+
+	for (json::iterator it = j.begin(); it != j.end(); ++it) {
+		json sug = it.value();
+		suggestion temp;
+		temp.id = sug["id"];
+		temp.name = sug["name"];
+		temp.cost = static_cast<int>(sug["cost"]);
+		v.push_back(temp);
+	}
+
+	std::sort(v.begin(), v.end(), suggestion::compare);
+	return v;
+}
+
+bool search(std::string item, std::string searchText) {
+	return (boost::contains(item, searchText));
+}
+
+
+
+
+
+
