b4be7f07dbe04c8b72ce7b49553f52ce081c2087
legik777 <75944488+legik777@users.noreply.github.com>
2021-04-05 22:39:13 +0300

    Update suggestions.hpp

--- include/suggestions.hpp
+++ include/suggestions.hpp
@@ -2,6 +2,9 @@
 #pragma once
 #include <vector>
 #include <string>
+#include <iostream>
+#include <fstream>
+#include <boost/algorithm/string.hpp>
 struct suggestion {
   std::string id;
   std::string name;
@@ -10,5 +13,28 @@ struct suggestion {
     return (sug1.cost > sug2.cost);
   }
 };
+std::vector<suggestion> getFromJson(std::string path) {
+  std::vector<suggestion> v;
+  std::ifstream i(path);
+  nlohmann::json j;
+  i >> j;
+
+  for (nlohmann::json::iterator it = j.begin(); it != j.end(); ++it) {
+    nlohmann::json sug = it.value();
+    suggestion temp;
+    temp.id = sug["id"];
+    temp.name = sug["name"];
+    temp.cost = static_cast<int>(sug["cost"]);
+    v.push_back(temp);
+  }
+
+  std::sort(v.begin(), v.end(), suggestion::compare);
+  return v;
+}
+
+bool search(std::string item, std::string searchText) {
+  return (boost::contains(item, searchText));
+}
+
 std::vector<suggestion> getFromJson(std::string path);
 bool search(std::string item, std::string searchText);
