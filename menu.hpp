ce53d04ded4256984431fe8fce6d3ebf292a0bd9
legik777 <75944488+legik777@users.noreply.github.com>
2021-04-18 18:17:53 +0300

    Create menu.hpp

---
+++ menu.hpp
@@ -0,0 +1,25 @@
+// Copyright 2020 Andreytorix
+#pragma once
+#include <boost/program_options.hpp>
+#include <string>
+class EApplication {
+public:
+  explicit EApplication(int argc, const char **argv);
+  int exec();
+
+private:
+  boost::program_options::options_description m_desk{"доступные опции"};
+  boost::program_options::variables_map m_vm;
+  /*
+  --url
+  ---depth
+--network_threads
+--parser_threads
+--output
+*/
+  std::string url;     //адрес сайта для парсинга
+  int depth;           //глубина поиска
+  int network_threads; //количество поток скачивания
+  int parser_threads;  //количество потоков парсера
+  std::string path;    //путь к файлу с результатом
+};
