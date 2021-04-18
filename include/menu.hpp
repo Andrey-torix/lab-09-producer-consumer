8435c0c035d612803736d363d3896d6b0d0dad89
legik777 <75944488+legik777@users.noreply.github.com>
2021-04-18 19:08:48 +0300

    Update menu.hpp

--- include/menu.hpp
+++ include/menu.hpp
@@ -1,25 +1,3 @@
 // Copyright 2020 Andreytorix
 #pragma once
-#include <boost/program_options.hpp>
-#include <string>
-class EApplication {
-public:
-  void menu(int argc, const char **argv);
-  int exec();
 
-private:
-  boost::program_options::options_description m_desk{"доступные опции"};
-  boost::program_options::variables_map m_vm;
-  /*
-  --url
-  ---depth
---network_threads
---parser_threads
---output
-*/
-  std::string url;     //адрес сайта для парсинга
-  int depth;           //глубина поиска
-  int network_threads; //количество поток скачивания
-  int parser_threads;  //количество потоков парсера
-  std::string path;    //путь к файлу с результатом
-};
