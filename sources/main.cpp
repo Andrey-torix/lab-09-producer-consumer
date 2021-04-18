8f0de8cd249f06f0600404619365945f729afa8a
legik777 <75944488+legik777@users.noreply.github.com>
2021-04-18 17:52:03 +0300

    Update main.cpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,7 +1,30 @@
 // Copyright 2020 Andreytorix
 #include <iostream>
 #include "../include/main.hpp"
-//#include <boost/program_options.hpp>
+#include <boost/program_options.hpp>
+#include <string>
+class EApplication {
+public:
+  explicit EApplication(int argc, const char **argv);
+  int exec();
+
+private:
+  boost::program_options::options_description m_desk{"доступные опции"};
+  boost::program_options::variables_map m_vm;
+  /*
+  --url
+  ---depth
+--network_threads
+--parser_threads
+--output
+*/
+  std::string url;     //адрес сайта для парсинга
+  int depth;           //глубина поиска
+  int network_threads; //количество поток скачивания
+  int parser_threads;  //количество потоков парсера
+  std::string path;    //путь к файлу с результатом
+};
+
 int main(int argc, const char* argv[])
 {
     //setlocale(LC_ALL, "Russian");
