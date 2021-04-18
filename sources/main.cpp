1671722e1cf22c823e1e4a02f89acff0d5f919af
legik777 <75944488+legik777@users.noreply.github.com>
2021-04-18 17:14:08 +0300

    Update main.cpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,10 +1,10 @@
 // Copyright 2020 Andreytorix
 #include <iostream>
 #include "main.hpp"
-#include <boost/program_options.hpp>
+//#include <boost/program_options.hpp>
 int main(int argc, const char* argv[])
 {
-    setlocale(LC_ALL, "Russian");
+    //setlocale(LC_ALL, "Russian");
     EApplication app(argc, argv);
     return app.exec();
 }
